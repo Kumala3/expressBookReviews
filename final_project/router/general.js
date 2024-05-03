ce7379d1198a6e49608b505ea63553f0216f1dd9
Kostek <ksytnik73kwer@gmail.com>
2024-05-03 18:40:27 +0200

    Refactor book GET endpoints to use axios and Async/Await for fetching book data

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const axios = require("axios");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -67,49 +68,48 @@ public_users.post("/login", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
-    return res.status(200).json(books);
+public_users.get("/", async function (req, res) {
+    try {
+        const response = await axios.get("");
+        res.status(200).json(response.data);
+    } catch (err) {
+        res.status(500).json({ message: "Failed to fetch books" });
+    }
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
+public_users.get("/isbn/:isbn", async function (req, res) {
     const isbn = req.params.isbn;
-    const book = books[isbn];
 
-    if (!book) {
-        return res.status(404).json({ message: "Book not found" });
-    } else {
-        return res.status(200).json(book);
+    try {
+        const response = await axios.get(`/${isbn}`);
+        return res.status(200).json(response.data);
+    } catch (err) {
+        return res.status(500).json({ message: "Failed to fetch book" });
     }
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
+public_users.get("/author/:author", async function (req, res) {
     const author = req.params.author.toLowerCase();
 
-    const book = Object.values(books).find(
-        book => book.author.toLowerCase() === author
-    );
-
-    if (!book) {
-        return res.status(404).json({ message: "Book not found" });
-    } else {
-        return res.status(200).json(book);
+    try {
+        const response = await axios.get(`/${author}`);
+        return res.status(200).json(response.data);
+    } catch (err) {
+        res.status(500).json({ message: "Failed to fetch book" });
     }
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
+public_users.get("/title/:title", async function (req, res) {
     const title = req.params.title.toLowerCase();
 
-    const book = Object.values(books).find(
-        book => book.title.toLowerCase() === title
-    );
-
-    if (!book) {
-        return res.status(404).json({ message: "Book not found" });
-    } else {
-        return res.status(200).json(book);
+    try {
+        const response = await axios.get(`/${title}`);
+        return res.status(200).json(response.data);
+    } catch (err) {
+        res.status(500).json({ message: "Failed to fetch book" });
     }
 });
 
