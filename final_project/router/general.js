03cab9135815ef8bb56c1b5b5ae1c2592b5525d3
Kostek <ksytnik73kwer@gmail.com>
2024-05-03 12:39:02 +0200

    Implement GET all books and GET book details based on ISBN endpoints

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,20 +6,25 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
-    // Write your code here
     return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
-    // Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
-    // Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const isbn = req.params.isbn;
+
+    const book = books[isbn];
+
+    if (!book) {
+        return res.status(404).json({ message: "Book not found" });
+    } else {
+        return res.status(200).json(book);
+    }
 });
 
 // Get book details based on author
