69d6c07e6dc1fe3006bb15a9b50f644b84d08aa6
Kostek <ksytnik73kwer@gmail.com>
2024-05-03 14:30:56 +0200

    Refactor authentication routes and update variable name in auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
-const regd_users = express.Router();
+
+const reg_users = express.Router();
 
 let users = [];
 
@@ -11,22 +12,44 @@ const isValid = username => {
 };
 
 const authenticatedUser = (username, password) => {
-    // Returns boolean
-    // Write code to check if username and password match the one we have in records.
+    const user = users.find(user => {
+        user.username === username && user.password === password;
+    });
+    return user != null;
 };
 
 // Only registered users can login
-regd_users.post("/login", (req, res) => {
-    // Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+reg_users.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        return res
+            .status(400)
+            .json({ message: "Username or password is incorrect" });
+    }
+
+    if (authenticatedUser(username, password)) {
+        const accessToken = jwt.sign({ data: password }, "access", {
+            expiresIn: "24h",
+        });
+
+        req.session.authorization = { accessToken };
+
+        return res
+            .status(200)
+            .json({ message: "User successfully logged in!" });
+    } else {
+        return res.status(403).json({ message: "User not authenticated" });
+    }
 });
 
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
+reg_users.put("/auth/review/:isbn", (req, res) => {
     // Write your code here
     return res.status(300).json({ message: "Yet to be implemented" });
 });
 
-module.exports.authenticated = regd_users;
+module.exports.authenticated = reg_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
